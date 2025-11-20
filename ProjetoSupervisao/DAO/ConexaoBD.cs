d523374e16e5cf9f907f70295c21519d90682636
Guilherme Mattos <guilhermeomattos@icloud.com>
2025-11-20 13:56:44 -0300

    Update ConexaoBD.cs

--- ProjetoSupervisao/DAO/ConexaoBD.cs
+++ ProjetoSupervisao/DAO/ConexaoBD.cs
@@ -6,7 +6,7 @@ namespace ProjetoSupervisao.DAO
     {
         public static SqlConnection GetConexao()
         {
-            string strCon = "Data Source=LOCALHOST; Initial Catalog=AMBIENTEDB; user id=sa; password=123456";
+            string strCon = "Server = tcp:n2server.database.windows.net,1433; Initial Catalog = AMBIENTEDB; Persist Security Info = False; User ID = engenharia; Password = eng@123456 ; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30";
 
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
